8304c9c55e92c897e4c2725d8f0088a646021d7b
Emil Eriksson <asheidan@gmail.com>
2026-09-20 19:20:39 +0200

    Add conform.nvim

--- init.lua
+++ init.lua
@@ -804,6 +804,20 @@ require("lazy").setup({
 		ft = "lua",
 		opts = {},
 	},
+	{
+		"stevearc/conform.nvim",
+		opts = {
+			formatters_by_ft = {
+				cpp = { "clang-format" },
+				lua = { "stylua" },
+				rust = { "rustfmt" },
+			},
+			format_on_save = {
+				timeout_ms = 500,
+				lsp_format = "fallback",
+			},
+		},
+	},
 	--{  -- Automatically display diagnostics
 	--	-- This seems to be broken
 	--	'dgagn/diagflow.nvim',
